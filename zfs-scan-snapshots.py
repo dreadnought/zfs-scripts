4acc9f1766d15e41a63684b82aecbeb14b92b388
Patrick Salecker <mail@salecker.org>
2018-08-05 12:09:09 +0200

    enhancements for zfs-scan-snapshots.py

--- zfs-scan-snapshots.py
+++ zfs-scan-snapshots.py
@@ -4,6 +4,9 @@ import subprocess
 import collections
 import time
 import hashlib
+import sys
+
+megabyte = 1024 * 1024
 
 def get_output(cmd, timeout=0, timeout_ok=False):
 	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
@@ -22,6 +25,8 @@ def get_output(cmd, timeout=0, timeout_ok=False):
 		lines.append(line)
 	stderr = proc.stderr.readlines()
 	if len(stderr) > 0:
+		if len(stderr) == 1 and stderr[0].startswith('Unable to determine path or stats for object'):
+			return False, ['error-ok', len(lines)]
 		print("Error:", stderr)
 		return False, ['error']
 
@@ -48,25 +53,39 @@ def scan_snapshots(volume, snapshots, cache):
 	if len(snapshots) < 3:
 		print('has less than 3 snapshots')
 		return todo
-	first_snapshot = snapshots[0]
-	last_snapshot = snapshots[-1]
+	first_snapshot = snapshots[0][0]
+	last_snapshot = snapshots[-1][0]
 	previous_snapshot = first_snapshot
-	for snapshot in snapshots:
+	previous_used = int(snapshots[0][1])
+	for snapshot, used in snapshots:
 		if snapshot.startswith('week_'):
 			last_weekly_snapshot = snapshot
 	print(first_snapshot, '...', last_snapshot)
-	for snapshot in snapshots[1:-2]:
-		print("-", snapshot, end="\t")
+	x = 0
+	num_snapshots_str = "%s" % (len(snapshots) - 3)
+	for snapshot, used in snapshots[1:-2]:
+		x += 1
+		x_str = "%s%s" % (" " * (len(num_snapshots_str) - len(str(x))), x)
+		print('[%s/%s]' % (x_str, num_snapshots_str), "-", snapshot, end="\t")
+		# STAGE 0: read from cache
 		command = ['/sbin/zfs', 'diff', "%s@%s" % (volume, previous_snapshot), "%s@%s" % (volume, snapshot)]
 		command_hash = hash_key(' '.join(command))
 		cache_result = get_from_cache(command_hash, cache)
+		used_diff = used - previous_used
 		if cache_result:
 			print('(cached)', end=" ")
 			success = True
-			if cache_result.startswith('>'):
+			if not cache_result.isdigit():
 				zfs_diff_len = cache_result
 			else:
 				zfs_diff_len = int(cache_result)
+		# STAGE 1: compare the used size
+		elif used_diff < megabyte * -1 or used_diff > megabyte * 1:
+			#add_to_cache(command_hash, cache_value, cache)
+			success = True
+			cache_result = True
+			zfs_diff_len = 'by-size-change %s Byte' % (used_diff)
+		# STAGE 2: use "zfs diff" for snapshots where the used size stayed the same
 		else:
 			success, output_lines = get_output(command, timeout=120, timeout_ok=True)
 			if success:
@@ -74,6 +93,9 @@ def scan_snapshots(volume, snapshots, cache):
 		if not success:
 			if 'timeout' in output_lines:
 				add_to_cache(command_hash, '>0', cache)
+			elif 'error-ok' in output_lines:
+				print("error-ok", len(output_lines))
+				add_to_cache(command_hash, ">%s" % len(output_lines), cache)
 			elif 'error' in output_lines:
 				pass
 		elif zfs_diff_len != 0:
@@ -97,7 +119,8 @@ def scan_snapshots(volume, snapshots, cache):
 			elif not cache_result:
 				add_to_cache(command_hash, zfs_diff_len, cache)
 		previous_snapshot = snapshot
-		print("")
+		previous_used = used
+		print("", flush=True)
 	return todo
 
 def load_cache(cache_file):
@@ -126,20 +149,28 @@ success, zfs_list = get_output(['/sbin/zfs', 'list', '-H', '-o', 'name'])
 volumes = collections.OrderedDict()
 todo = []
 cache_file = '/var/cache/zfs-snapshots-diff.txt'
+if len(sys.argv) == 2:
+	filter_volume = sys.argv[1]
+else:
+	filter_volume = None
 
 for line in zfs_list:
 	volumes[line] = {'snapshots': []}
 
-success, zfs_list_snapshots = get_output(['/sbin/zfs', 'list', '-H', '-o', 'name', '-t', 'snapshot'])
+success, zfs_list_snapshots = get_output(['/sbin/zfs', 'list', '-H', '-p', '-o', 'name,used', '-t', 'snapshot'])
 for line in zfs_list_snapshots:
-	volume, snapshot = line.split('@', 1)
+	name, used = line.split('\t', 1)
+	volume, snapshot = name.split('@', 1)
 	if snapshot.startswith('rsync_'):
 		continue
-	volumes[volume]['snapshots'].append(snapshot)
+	volumes[volume]['snapshots'].append([snapshot, int(used)])
 
 cache = load_cache(cache_file)
 for volume in volumes:
 	print(volume, end=" ")
+	if filter_volume and not volume.startswith("%s" % filter_volume):
+		print("skip by filter")
+		continue
 	snapshots = volumes[volume]['snapshots']
 	try:
 		volume_todo = scan_snapshots(volume, snapshots, cache)
@@ -147,6 +178,7 @@ for volume in volumes:
 		print("interrupted")
 		break
 	todo.extend(volume_todo)
+	print("", flush=True)
 
 if len(todo) > 0:
 	print("Commands to delete empty snapshots:")
